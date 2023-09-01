d23c20b18318e45a159158c1bba8e90fb28c98e0
dduneon <dduneon@gmail.com>
2023-09-01 16:50:49 +0900

    [Todo6] NhnMart.java getFoodStand() 구현

--- src/main/java/com/nhnacademy/mart/FoodStand.java
+++ src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -6,9 +6,12 @@ public class FoodStand {
 
     private final ArrayList<Food> foods = new ArrayList<>();
 
-    // TODO add 메서드 구현
+    // TODO add 메서드 구현 (Complete)
     public void add(Food food) {
         foods.add(food);
     }
-    // TODO 장바구니에 담은 Food 삭제 구현
+    // TODO 장바구니에 담은 Food 삭제 구현 (Not yet)
+    public void remove() {
+        foods.clear();
+    }
 }
