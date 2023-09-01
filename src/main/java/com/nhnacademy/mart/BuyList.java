ed1d7592baeb6f2540711a5fe8a345ecd4c064e5
dduneon <dduneon@gmail.com>
2023-09-01 16:37:01 +0900

    [Todo3] BuyList.java add() 메서드 작성

--- src/main/java/com/nhnacademy/mart/BuyList.java
+++ src/main/java/com/nhnacademy/mart/BuyList.java
@@ -7,12 +7,17 @@ public class BuyList {
     private final ArrayList<Item> items = new ArrayList<>();
 
     // TODO add 메서드 생성
-
+    public void add(String name, int amount) {
+        items.add(new Item(name, amount));
+    }
 
     public static class Item {
         private final String name;
         private final int amount;
-
+        public Item(String name, int amount) {
+            this.name = name;
+            this.amount = amount;
+        }
 
     }
 }
