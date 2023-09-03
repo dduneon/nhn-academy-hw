c75f379cca8c54c30496398244725761fae3fab7
dduneon <dduneon@gmail.com>
2023-09-03 22:33:31 +0900

    [Todo16] Logback 추가 및 주석 추가

--- src/main/java/com/nhnacademy/mart/Customer.java
+++ src/main/java/com/nhnacademy/mart/Customer.java
@@ -21,11 +21,16 @@ public class Customer {
         this.basket = basket;
     }
 
-    // TODO pickFoods 메서드 구현 (Complete)
-    // TODO add 메서드 추가 및 remove 옮기기 구현하기
+    /**
+     * 구매목록에 있는 제품들을 장바구니로 옮기는 메서드
+     * 존재하지 않는 상품명이라면 에러를 발생시키고 종료
+     * 상품 수량이 부족한 경우 에러를 발생시키고 종료
+     * @param foodStand
+     */
     public void pickFoods(FoodStand foodStand) {
         for (BuyList.Item item: buyList.getItems()) {
             Food tmp;
+            // getName()의 return이 null이면 해당 상품이 존재하지 않는 것임
             if ((tmp = foodStand.findFood(item.getName())) == null) {
                 Logback.logger.error("Customer.pickfood() : 존재하지 않는 상품명, {} 입력", item.getName());
                 throw new IllegalArgumentException("존재하지 않는 상품명을 입력하셨습니다");
@@ -34,8 +39,8 @@ public class Customer {
                 if(foodStand.remove(tmp)) {
                     basket.add(tmp);
                 } else {
-                    Logback.logger.error("Customer.pickFood() : {} 상품이 존재하지 않습니다", tmp.getName());
-                    System.out.println(tmp.getName() + " 상품이 존재하지 않습니다. 확인 해주세요!");
+                    Logback.logger.error("Customer.pickFood() : {} 상품 수량이 더이상 존재하지 않습니다", tmp.getName());
+                    System.out.println(tmp.getName() + " 상품 수량이 존재하지 않습니다. 확인 해주세요!");
                 }
             }
         }
--- src/main/java/com/nhnacademy/mart/FoodStand.java
+++ src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -6,11 +6,9 @@ public class FoodStand {
 
     private final ArrayList<Food> foods = new ArrayList<>();
 
-    // TODO add 메서드 구현 (Complete)
     public void add(Food food) {
         foods.add(food);
     }
-    // TODO 장바구니에 담은 Food 삭제 구현 (Complete)
     public boolean remove(Food food) {
         return foods.remove(food);
     }
