c1caffff607df6e0c656ea02e8687e78c1ff7cee
dduneon <dduneon@gmail.com>
2023-09-03 22:13:45 +0900

    [Todo11] Logback 추가 및 오류 수정

---
+++ src/main/java/com/nhnacademy/mart/Logback.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.mart;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Logback {
+    public static final Logger logger = LoggerFactory.getLogger(Logback.class);
+}
