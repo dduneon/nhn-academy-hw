d32755d1ae652abab13d84337bace105ce4371d5
dduneon <dduneon@gmail.com>
2023-09-03 22:16:28 +0900

    [Todo13] Counter.java, Customer.java payTox() 예외처리 추가

--- src/main/java/com/nhnacademy/mart/Counter.java
+++ src/main/java/com/nhnacademy/mart/Counter.java
@@ -5,8 +5,10 @@ public class Counter {
     // TODO pay 메서드 구현 (카운터에서 계산 처리 메서드)
     public void pay(int total, Customer customer) {
         int rest = customer.getMoney() - total;
-        if (rest < 0)
+        if (rest < 0) {
+            Logback.logger.error("Counter.pay() : 잔액이 부족합니다. 총 가격 = {}, 잔액 {}", total, customer.getMoney());
             throw new IllegalArgumentException("잔액이 부족합니다.");
+        }
         System.out.println("총 가격은 " + customer.getMoney() + "입니다.");
         System.out.println("고객님 결제 후 잔액 : " + rest);
         customer.setMoney(rest);
--- src/main/java/com/nhnacademy/mart/Customer.java
+++ src/main/java/com/nhnacademy/mart/Customer.java
@@ -13,7 +13,7 @@ public class Customer {
 
     public Customer(BuyList buyList) {
         this.buyList = buyList;
-        setMoney(20000);
+        this.money = 20000;
     }
 
     // 장바구니 챙기기
@@ -24,15 +24,32 @@ public class Customer {
     // TODO pickFoods 메서드 구현 (Complete)
     // TODO add 메서드 추가 및 remove 옮기기 구현하기
     public void pickFoods(FoodStand foodStand) {
-        foodStand.remove(basket);
-        buyList.add();
+        for (BuyList.Item item: buyList.getItems()) {
+            Food tmp;
+            if ((tmp = foodStand.findFood(item.getName())) == null) {
+                Logback.logger.error("Customer.pickfood() : 존재하지 않는 상품명, {} 입력", item.getName());
+                throw new IllegalArgumentException("존재하지 않는 상품명을 입력하셨습니다");
+            }
+            for (int i=0; i<item.getAmount(); i++) {
+                if(foodStand.remove(tmp)) {
+                    basket.add(tmp);
+                } else {
+                    Logback.logger.error("Customer.pickFood() : {} 상품이 존재하지 않습니다", tmp.getName());
+                    System.out.println(tmp.getName() + " 상품이 존재하지 않습니다. 확인 해주세요!");
+                }
+            }
+        }
     }
 
 
 
     // TODO payTox 메서드 구현
     public void payTox(Counter counter) {
-        counter.pay(this.basket.getTotalAmount(), this);
+        try {
+            counter.pay(this.basket.getTotalAmount(), this);
+        } catch (IllegalArgumentException e) {
+            System.out.println("잔액이 부족합니다! 계산할 총 금액 : " + basket.getTotalAmount() + ", 잔액 : " + getMoney());
+        }
     }
 
     // Money getter() / setter()
