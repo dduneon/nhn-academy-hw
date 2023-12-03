f6ed2e5dcef1bb5bda069d95ceae0ba32e397757
dduneon <dduneon@gmail.com>
2023-12-03 23:09:58 +0900

    todo #4 complete

--- src/main/java/com/nhnacademy/shoppingmall/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/nhnacademy/shoppingmall/user/service/impl/UserServiceImpl.java
@@ -1,42 +1,91 @@
 package com.nhnacademy.shoppingmall.user.service.impl;
 
-import com.nhnacademy.shoppingmall.user.service.UserService;
 import com.nhnacademy.shoppingmall.user.domain.User;
+import com.nhnacademy.shoppingmall.user.exception.UserAlreadyExistsException;
+import com.nhnacademy.shoppingmall.user.exception.UserNotFoundException;
 import com.nhnacademy.shoppingmall.user.repository.UserRepository;
+import com.nhnacademy.shoppingmall.user.service.UserService;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
+  private final UserRepository userRepository;
+
+  public UserServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public User getUser(String userId) {
+    //todo#4-1 회원조회
+    Optional<User> user = userRepository.findById(userId);
+    return user.orElse(null);
+  }
+
+  @Override
+  public void saveUser(User user) {
+    //todo#4-2 회원등록
+    if (Objects.isNull(user)) {
+      throw new RuntimeException("user is null");
     }
-
-    @Override
-    public User getUser(String userId){
-        //todo#4-1 회원조회
-        return null;
+    if (isExistUser(user.getUserId())) {
+      throw new UserAlreadyExistsException("user already exist");
     }
-
-    @Override
-    public void saveUser(User user) {
-        //todo#4-2 회원등록
-
+    int result = userRepository.save(user);
+    if (result < 1) {
+      throw new RuntimeException("Do not save user");
     }
+  }
 
-    @Override
-    public void updateUser(User user) {
-        //todo#4-3 회원수정
+  @Override
+  public void updateUser(User user) {
+    //todo#4-3 회원수정
+    if (Objects.isNull(user)) {
+      throw new RuntimeException("user is null");
     }
-
-    @Override
-    public void deleteUser(String userId) {
-        //todo#4-4 회원삭제
+    if (!isExistUser(user.getUserId())) {
+      throw new UserNotFoundException("user doesn't exist");
     }
-
-    @Override
-    public User doLogin(String userId, String userPassword) {
-        //todo#4-5 로그인 구현, userId, userPassword로 일치하는 회원 조회
-        return null;
+    int result = userRepository.update(user);
+    if (result < 1) {
+      throw new RuntimeException("Do not update user");
     }
+  }
 
+  @Override
+  public void deleteUser(String userId) {
+    //todo#4-4 회원삭제
+    if (!isExistUser(userId)) {
+      throw new UserNotFoundException("user doesn't exist");
+    }
+    int result = userRepository.deleteByUserId(userId);
+    if (result < 1) {
+      throw new RuntimeException("Do not update user");
+    }
+  }
+
+  @Override
+  public User doLogin(String userId, String userPassword) {
+    //todo#4-5 로그인 구현, userId, userPassword로 일치하는 회원 조회
+    Optional<User> user = userRepository.findByUserIdAndUserPassword(userId, userPassword);
+    if (user.isEmpty() || Objects.isNull(user)) {
+      throw new UserNotFoundException("user doesn't exist");
+    }
+    // 최근 로그인 시간을 현재 시간으로 변경
+    userRepository.updateLatestLoginAtByUserId(userId, LocalDateTime.now());
+    return user.get();
+  }
+
+  /**
+   * userRepository에 존재하는 user인지 판단하는 메서드
+   *
+   * @param userId 조회할 user의 id
+   * @return 유저가 존재하면 true, 존재하지 않으면 false
+   */
+  public boolean isExistUser(String userId) {
+    int count = userRepository.countByUserId(userId);
+    return count > 0;
+  }
 }
