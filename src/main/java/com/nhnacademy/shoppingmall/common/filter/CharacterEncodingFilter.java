e7a21ef7037f3c943a0996274b9740a6a4ac742c
dduneon <dduneon@gmail.com>
2023-12-04 16:31:15 +0900

    todo #8 complete

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
@@ -3,26 +3,35 @@ package com.nhnacademy.shoppingmall.common.filter;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @WebFilter(filterName = "characterEncodingFilter", initParams = {
     @WebInitParam(name = "encoding", value = "UTF-8")
-})
+}, urlPatterns = "/*")
 public class CharacterEncodingFilter implements Filter {
+
+  private String encoding;
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    this.encoding = filterConfig.getInitParameter("encoding");
+  }
 
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
       FilterChain filterChain) throws IOException, ServletException {
     //todo#8 UTF-8 인코딩, initParams의 encoding parameter value값을 charset 으로 지정합니다.
-    servletRequest.setCharacterEncoding(
-        servletRequest.getServletContext().getInitParameter("encoding"));
-    servletResponse.setCharacterEncoding(
-        servletRequest.getServletContext().getInitParameter("encoding"));
+    log.debug("doFilter called");
+    servletRequest.setCharacterEncoding(encoding);
+    servletResponse.setCharacterEncoding(encoding);
     filterChain.doFilter(servletRequest, servletResponse);
+    log.debug("doFilter after");
   }
-
 }
