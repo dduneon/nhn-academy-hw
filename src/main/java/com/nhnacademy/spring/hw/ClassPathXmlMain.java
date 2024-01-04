ffe7e0a933727ccfdf440a8d5397c4b77ec5bd7e
dduneon <dduneon@gmail.com>
2024-01-04 16:50:26 +0900

    init

--- src/main/java/com/nhnacademy/spring/hw/ClassPathXmlMain.java
+++ src/main/java/com/nhnacademy/spring/hw/ClassPathXmlMain.java
@@ -1,11 +1,16 @@
 package com.nhnacademy.spring.hw;
 
+import com.nhnacademy.spring.hw.sender.MessageSender;
+import com.nhnacademy.spring.hw.service.MessageSendService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ClassPathXmlMain {
   public static void main(String[] args) {
 
-    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+    try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+      MessageSendService messageSendService = context.getBean("messageSendService", MessageSendService.class);
+      messageSendService.doSendMessage();
+    }
 
   }
 }
