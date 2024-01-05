e99124af82dfff8023a89159f6713e8b2a852d06
dduneon <dduneon@gmail.com>
2024-01-05 13:22:53 +0900

    (-) 쓸데없는 주석 삭제

--- src/test/java/com/nhnacademy/spring/hw/sender/DoorayMessageSenderTest.java
+++ src/test/java/com/nhnacademy/spring/hw/sender/DoorayMessageSenderTest.java
@@ -33,7 +33,6 @@ public class DoorayMessageSenderTest {
 
   @Test
   void testSendMessageWithNullParameters() {
-    // messageSender.sendMessage가 특정 매개변수에 대해 RuntimeException을 throw하도록 설정
     when(messageSender.sendMessage(null, null)).thenThrow(new IllegalArgumentException("Invalid parameters"));
 
     boolean actual = messageSendService.doSendMessage(null, null);
@@ -42,7 +41,6 @@ public class DoorayMessageSenderTest {
 
   @Test
   void testSendMessageWithEmptyParameters() {
-    // messageSender.sendMessage가 특정 매개변수에 대해 RuntimeException을 throw하도록 설정
     when(messageSender.sendMessage("", "")).thenThrow(new IllegalArgumentException("Invalid parameters"));
 
     boolean actual = messageSendService.doSendMessage("", "");
