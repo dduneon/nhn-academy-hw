fa540c821526e54e9f65c79bbf16b352f0f533dc
dduneon <dduneon@gmail.com>
2024-01-05 13:17:04 +0900

    (*) AspectConfig @EnableAspectJAutoProxy add

--- src/main/java/com/nhnacademy/spring/hw/config/AspectConfig.java
+++ src/main/java/com/nhnacademy/spring/hw/config/AspectConfig.java
@@ -3,8 +3,10 @@ package com.nhnacademy.spring.hw.config;
 import com.nhnacademy.spring.hw.aspect.CheckExecutionTimeAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class AspectConfig {
   @Bean(name = "checkExecutionTimeAspect")
   CheckExecutionTimeAspect checkExecutionTimeAspect() {
