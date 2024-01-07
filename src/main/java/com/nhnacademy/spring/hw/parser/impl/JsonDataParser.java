e45e216ccec153e6ee50417c56f13f886ffdd26f
dduneon <dduneon@gmail.com>
2024-01-07 21:02:25 +0900

    (+) JsonParser temporary add

---
+++ src/main/java/com/nhnacademy/spring/hw/parser/impl/JsonDataParser.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.spring.hw.parser.impl;
+
+import com.nhnacademy.spring.hw.model.WaterTariff;
+import com.nhnacademy.spring.hw.parser.DataParser;
+import java.util.List;
+
+public class JsonDataParser implements DataParser {
+
+  @Override
+  public List<WaterTariff> parse(String filePath) {
+    return null;
+  }
+}
