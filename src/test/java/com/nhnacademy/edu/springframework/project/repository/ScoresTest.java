17a8bba9dc11f74e0d331abf9811c5f3441db245
dduneon <dduneon@gmail.com>
2024-01-08 04:37:19 +0900

    (+) add test cases

--- src/test/java/com/nhnacademy/edu/springframework/project/repository/ScoresTest.java
+++ src/test/java/com/nhnacademy/edu/springframework/project/repository/ScoresTest.java
@@ -2,18 +2,11 @@ package com.nhnacademy.edu.springframework.project.repository;
 
 import com.nhnacademy.edu.springframework.project.config.ComponentConfig;
 import com.nhnacademy.edu.springframework.project.domain.Score;
-import com.nhnacademy.edu.springframework.project.repository.impl.CsvScores;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.csv.Csv;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
