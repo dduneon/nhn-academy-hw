44de5de1fb5721fcae99a87068c031b916b0b534
dduneon <dduneon@gmail.com>
2024-01-12 15:48:01 +0900

    (+) polymolphism to user

---
+++ src/main/java/com/nhnacademy/springmvc/domain/Role.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.springmvc.domain;
+
+public enum Role {
+  User, Admin
+}
--- src/main/java/com/nhnacademy/springmvc/domain/User.java
+++ src/main/java/com/nhnacademy/springmvc/domain/User.java
@@ -1,7 +1,16 @@
 package com.nhnacademy.springmvc.domain;
 
-public class User {
-  String id;
-  String password;
-  String name;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+public class User implements Serializable {
+  private String id;
+  private String password;
+  private String name;
+  private Role role;
 }
