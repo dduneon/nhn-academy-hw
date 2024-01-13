a5a4e7979f8e7dcbd232eeda398446693f0c7587
dduneon <dduneon@gmail.com>
2024-01-14 06:09:25 +0900

    [ADD] Admin answer page add

--- src/main/java/com/nhnacademy/springmvc/controller/AdminAnswerController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/AdminAnswerController.java
@@ -1,15 +1,32 @@
 package com.nhnacademy.springmvc.controller;
 
+import com.nhnacademy.springmvc.domain.Inquiry;
+import com.nhnacademy.springmvc.exception.InquiryNotFoundException;
+import com.nhnacademy.springmvc.service.AdminInquiryService;
+import java.util.Objects;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cs/admin/answer")
 public class AdminAnswerController {
-  @GetMapping
-  public String getAdminAnswer() {
+  private final AdminInquiryService adminInquiryService;
+
+  public AdminAnswerController(AdminInquiryService adminInquiryService) {
+    this.adminInquiryService = adminInquiryService;
+  }
+
+  @GetMapping("/{inquiryId}")
+  public String getAdminAnswer(@PathVariable(name = "inquiryId") long inquiryId, Model model) {
+    Inquiry inquiry = adminInquiryService.getInquiryById(inquiryId);
+    if(Objects.isNull(inquiry))
+      throw new InquiryNotFoundException();
+
+    model.addAttribute("inquiry", inquiry);
     return "admin/answer";
   }
 
