8c90c9233c1ba92aaa191f298cc139df9add72cb
dduneon <dduneon@gmail.com>
2024-01-22 10:00:02 +0900

    [ADD] fin

--- src/main/java/com/nhnacademy/springjpa/config/RootConfig.java
+++ src/main/java/com/nhnacademy/springjpa/config/RootConfig.java
@@ -17,8 +17,8 @@ public class RootConfig {
     BasicDataSource dataSource = new BasicDataSource();
     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
     dataSource.setUrl("jdbc:mysql://133.186.241.167:3306/nhn_academy_13?serverTimezone=UTC&characterEncoding=UTF-8");
-    dataSource.setUsername("nhn_academy_13");
-    dataSource.setPassword("EOcV(baaATcu3lny");
+    dataSource.setUsername("nhn_academy_12");
+    dataSource.setPassword("jcR59.XEQFn[ES6o");
 
     dataSource.setInitialSize(10);
     dataSource.setMaxTotal(50);
