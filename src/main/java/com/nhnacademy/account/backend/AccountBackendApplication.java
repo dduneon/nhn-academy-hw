1ff5777138756b2fb8a8e869b62284bf2a79ba7c
dduneon <dduneon@gmail.com>
2024-01-23 19:05:57 +0900

    [INIT] init proj

---
+++ src/main/java/com/nhnacademy/account/backend/AccountBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.account.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBackendApplication.class, args);
+	}
+
+}
