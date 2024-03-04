b7bc7c06be522f8665ee8dd7f5c1d5e707c906c7
dduneon <dduneon@gmail.com>
2024-03-04 19:53:31 +0900

    restore

--- nhn-servlet-hw/src/main/java/com/nhnacademy/shoppingmall/categoryproduct/service/CategoryProductService.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.shoppingmall.categoryproduct.service;
-
-import java.util.List;
-
-public interface CategoryProductService {
-  List<String> getCategoriesName(int productId);
-}
--- nhn-servlet-hw/src/main/java/com/nhnacademy/shoppingmall/orderdetails/repository/OrderDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.shoppingmall.orderdetails.repository;
-
-import com.nhnacademy.shoppingmall.orderdetails.domain.OrderDetails;
-
-public interface OrderDetailsRepository {
-
-  int save(OrderDetails orderDetails);
-}
